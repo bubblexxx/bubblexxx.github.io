894382b0f5ef885e28a0cd2ba08ab721a23745e0
gregory <espace3d@gmail.com>
2017-05-09 16:58:20 +0200

    screencanvas bas

--- src/main.js
+++ src/main.js
@@ -7,12 +7,6 @@
 //explode dalle neon asteroid pulsar
 //changer couleur particle d'asteroid
 //detecter si gsm ou ordi et régler admob et cordova en fonction
-var canvas = document.createElement("canvas");
-canvas.screencanvas = true;
-canvas.width = window.innerWidth * window.devicePixelRatio;
-canvas.height = window.innerHeight * window.devicePixelRatio;
-var ctx = canvas.getContext("experimental-webgl");	
-	console.log('without_screen_canvas+')
 
 function main(){
 	
@@ -1846,6 +1840,12 @@ function main(){
 			}
 		}
 	}
+	var canvas = document.createElement("canvas");
+	canvas.screencanvas = true;
+	canvas.width = window.innerWidth * window.devicePixelRatio;
+	canvas.height = window.innerHeight * window.devicePixelRatio;
+	var ctx = canvas.getContext("experimental-webgl");	
+	console.log('without_screen_canvas+')
 	//game = new Phaser.Game(640,960,Phaser.CANVAS,'game' )
 	game = new Phaser.Game(1280,1920,Phaser.CANVAS,'game' )
 	//game = new Phaser.Game(1280,1920,Phaser.AUTO,'game' )
