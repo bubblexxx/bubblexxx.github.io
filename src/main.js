3b156aa2e7ef64acb2335c1382fbd226bb810a99
espace3d <espace3d@gmail.com>
2017-11-08 15:18:25 +0100

    tentative charboost a

--- src/main.js
+++ src/main.js
@@ -110,7 +110,7 @@ var adService;
 var detectmob=function(){ 
 	if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)
 	){
-		alert("Mobile")
+		alert("mobile")
 		console.log("mobile")
 		is_mobile=true;
 	} else {
@@ -591,7 +591,7 @@ character.prototype.preload_reward_video=function(){
 	};
 	window.chartboost.onRewardedVideoAdLoaded = function(location) {
 		alert('onRewardedVideoAdLoaded: ' + location);
-		is_rewarded_video_completed && this.game.state.start('intermediate_screen');
+		//is_rewarded_video_completed && this.game.state.start('intermediate_screen');
 	};
 	window.chartboost.onRewardedVideoAdShown = function(location) {
 		//alert('onRewardedVideoAdShown: ' + location);
@@ -599,9 +599,10 @@ character.prototype.preload_reward_video=function(){
 	window.chartboost.onRewardedVideoAdHidden = function(location) {
 		alert('onRewardedVideoAdHidden: ' + location);
 		is_rewarded_video_completed=true;
-		this.game.state.start('intermediate_screen');
+		//this.game.state.start('intermediate_screen');
 	};
 	window.chartboost.onRewardedVideoAdCompleted = function(location) {
+		this.game.state.start('intermediate_screen');
 		//alert('onRewardedVideoAdCompleted: ' + location);
 		//ecran_intermediaire_pour_passer_level(background_to_pass_level,this.pass_level)
 		//this.next_niveau=level_number+1
